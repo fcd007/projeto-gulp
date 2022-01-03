f9840c8c62a7b471f6334c48c0a09f4ca5fcebf9
Claudeilton Dantas <fcd007@hotmail.com>
2022-01-03 17:54:18 -0300

    chore: add browser-sync and watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 function compilaSass() {
   return gulp
@@ -9,16 +10,29 @@ function compilaSass() {
     outputStyle: 'compressed'
   })) //modo compresso
   //.pipe(sass()) //modo padrão de saída - espaços e lixo junto
-  .pipe(gulp.dest('dist/css/'))
+  .pipe(gulp.dest('src/css'))
   .pipe(autoprefixer({
     cascade: false
   }))
+  .pipe(browserSync.stream());
 }
 //gulp.task("sass", compilaSass); //executar como $gulp sass
 gulp.task('sass', compilaSass); // executa oomo padrão $ gulp
+
+function browser() {
+  browserSync.init( {
+    server: {
+      baseDir: './src'
+    }
+  });
+}
+
+gulp.task('browser-sync', browser);
 
 function watch() {
   gulp.watch('src/scss/*.scss', compilaSass);
 }
 
-gulp.task('default', watch);
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
