d540b1155cf059220524d82b1dd4c505145dbab9
Claudeilton Dantas <fcd007@hotmail.com>
2022-01-03 20:59:45 -0300

    chore: ajuste file gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+//adiciona os plugins dos módulos instalados
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+//função para compilar o SASS e os prefixos
 function compilaSass() {
   return gulp
   .src('src/scss/*.scss')
@@ -17,8 +19,10 @@ function compilaSass() {
   .pipe(browserSync.stream());
 }
 //gulp.task("sass", compilaSass); //executar como $gulp sass
+//tarefa de Gulp para a função de SASS
 gulp.task('sass', compilaSass); // executa oomo padrão $ gulp
 
+//função para inicia ro browser
 function browser() {
   browserSync.init( {
     server: {
@@ -27,12 +31,17 @@ function browser() {
   });
 }
 
+//tarefa do browser-sync
 gulp.task('browser-sync', browser);
 
+//função de watch do Gulp
 function watch() {
   gulp.watch('src/scss/*.scss', compilaSass);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
 }
 
+//inicia a tarefa de watch
 gulp.task('watch', watch);
 
+//tarefa padrão do Gulp
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
