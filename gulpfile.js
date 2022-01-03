a812cf440b2ed8c76d500f5541fe6a11212443e8
Claudeilton Dantas <fcd007@hotmail.com>
2022-01-03 16:37:25 -0300

    chore: ajuste gulp arquivos projeto

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,10 @@ function compilaSass() {
     outputStyle: 'compressed'
   })) //modo compresso
   //.pipe(sass()) //modo padrão de saída - espaços e lixo junto
-  .pipe(gulp.dest('src/css/'))
+  .pipe(gulp.dest('dist/css/'))
   .pipe(autoprefixer({
     cascade: false
   }))
-  .pipe(gulp.dest('dist'))
 }
 
 //gulp.task("sass", compilaSass); //executar como $gulp sass
